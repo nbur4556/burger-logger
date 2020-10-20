2b059c8a8cc7349bb8ceb357848f23519b4f9034
nbur4556 <nbur4556@gmail.com>
2020-10-20 15:07:20 -0500

    sets up create prototype to add new data columns to database

--- config/orm.js
+++ config/orm.js
@@ -4,8 +4,16 @@ const Orm = function (table) {
     this.table = table;
 }
 
+// Create a new data column
+Orm.prototype.create = function (colNames, colValues, cb = function () { }) {
+    connection.query('INSERT INTO ??(??) VALUES(?)', [this.table, colNames, colValues], (err, data) => {
+        if (err) throw err;
+        this.selectAll(cb);
+    });
+}
+
 // Return all values from table
-Orm.prototype.selectAll = function (cb) {
+Orm.prototype.selectAll = function (cb = function () { }) {
     connection.query('SELECT * FROM ??', [this.table], (err, data) => {
         if (err) throw err;
         cb(data);
@@ -13,7 +21,7 @@ Orm.prototype.selectAll = function (cb) {
 }
 
 // Return all values where a data column equals a specific value
-Orm.prototype.selectWhere = function (colName, colValue, cb) {
+Orm.prototype.selectWhere = function (colName, colValue, cb = function () { }) {
     connection.query(`SELECT * FROM ?? WHERE ?? = ?`, [this.table, colName, colValue], (err, data) => {
         if (err) throw err;
         cb(data);
@@ -24,3 +32,7 @@ module.exports = Orm
 
 // TESTING
 const orm = new Orm('burgers');
+
+orm.create(["burger_name", "is_ready"], ["Italian", 1], data => {
+    console.log(data);
+});
