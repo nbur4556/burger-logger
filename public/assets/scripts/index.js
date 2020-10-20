1fa3159df1908ab2a6fb79aa6e5fcb335e306d7a
nbur4556 <nbur4556@gmail.com>
2020-10-20 16:03:33 -0500

    ajax call for list of burgers on page load

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -1,9 +1,18 @@
 $(document).ready(() => {
     $('#place-order').click(placeOrder);
+
+    // Get all current burgers from database
+    $.ajax({
+        url: '/api/burgers',
+        type: "GET"
+    }).then(data => {
+        console.log(data);
+    });
+
 });
 
 function placeOrder() {
     let burgerName = $('#burger-input').val();
 
-    $('.order-list').append(`<li> ${burgerName} <button class="ready-order">Ready</button> </li>`);
+    $('.order-list').append(`<li>${burgerName}</li>`);
 }
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const expHandlebars = require('express-handlebars');
+const Orm = require('./config/orm');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+const orm = new Orm('burgers');
 
 // Set handlebars template and view engine
 app.engine("handlebars", expHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/api/burgers', (req, res) => {
+    console.log('Getting burgers');
+    orm.selectAll(data => {
+        console.log(data);
+        res.json(data);
+    });
 });
 
 app.listen(PORT, () => {
