533a76ed25f81c8b4ebb3f5845b176dd2fd2bf30
nbur4556 <nbur4556@gmail.com>
2020-10-20 15:46:20 -0500

    place order button gets value from burger text box and adds burgername to order list

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -1,3 +1,9 @@
 $(document).ready(() => {
-
+    $('#place-order').click(placeOrder);
 });
+
+function placeOrder() {
+    let burgerName = $('#burger-input').val();
+
+    $('.order-list').append(`<li> ${burgerName} <button class="ready-order">Ready</button> </li>`);
+}
