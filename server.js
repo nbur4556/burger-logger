72b6d2eca114c2eb4f1cde4671c973603689e659
nbur4556 <nbur4556@gmail.com>
2020-10-20 10:55:19 -0500

    removed database testing queries, database succesfully deployed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const expHandlebars = require('express-handlebars');
-const connection = require('./config/connection');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,12 +11,7 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-    // res.render('index');
-
-    // Test Query
-    connection.query('SELECT * FROM burgers', (err, data) => {
-        res.send(data);
-    });
+    res.render('index');
 });
 
 app.listen(PORT, () => {
