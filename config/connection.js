e9281f9e27784a6103bc5f6994d8582f560a0ed1
nbur4556 <nbur4556@gmail.com>
2020-10-20 10:38:11 -0500

    removed config from deployment

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
-const config = require('./config.json');
+// const config = require('./config.json');
 
 const connection = mysql.createConnection({
-    host: process.env.CLEARDB_HOST || config.host,
-    user: process.env.CLEARDB_USER || config.user,
-    password: process.env.CLEARDB_PASSWORD || config.password,
-    database: process.env.CLEARDB_DATABASE || config.database
+    host: process.env.CLEARDB_HOST,
+    user: process.env.CLEARDB_USER,
+    password: process.env.CLEARDB_PASSWORD,
+    database: process.env.CLEARDB_DATABASE
 });
 
 connection.connect(err => {
