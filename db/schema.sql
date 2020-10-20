2fb593ee7de8542cb90d81a01f65efc17be5ef7f
nbur4556 <nbur4556@gmail.com>
2020-10-20 10:01:38 -0500

    created and populated burger database

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers(
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(30) NOT NULL,
+  is_ready BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+  burgers(burger_name)
+VALUES("Cheesburger");
+INSERT INTO
+  burgers(burger_name)
+VALUES("Bacon Burger");
+INSERT INTO
+  burgers(burger_name)
+VALUES("Super Deluxe");
+SELECT
+  *
+FROM
+  burgers;
