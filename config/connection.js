3a6a9f88589b4bd1512ced1ce427e062c3ab7bea
nbur4556 <nbur4556@gmail.com>
2020-10-22 18:18:10 -0500

    added port to database

--- config/connection.js
+++ config/connection.js
@@ -2,28 +2,31 @@ const mysql = require('mysql');
 
 const connectVars = new Array();
 
-// try {
-//     // If config file exists, push development credentials
-//     const config = require('./config.json');
-//     connectVars.push(config.host);
-//     connectVars.push(config.user);
-//     connectVars.push(config.password);
-//     connectVars.push(config.database);
-// }
-// catch {
-// Push deployment credentials
-connectVars.push(process.env.CLEARDB_HOST);
-connectVars.push(process.env.CLEARDB_USER);
-connectVars.push(process.env.CLEARDB_PASSWORD);
-connectVars.push(process.env.CLEARDB_DATABASE);
-// }
+try {
+    // If config file exists, push development credentials
+    const config = require('./config.json');
+    connectVars.push(config.host);
+    connectVars.push(config.user);
+    connectVars.push(config.password);
+    connectVars.push(config.database);
+    connectVars.push(3306);
+}
+catch {
+    // Push deployment credentials
+    connectVars.push(process.env.CLEARDB_HOST);
+    connectVars.push(process.env.CLEARDB_USER);
+    connectVars.push(process.env.CLEARDB_PASSWORD);
+    connectVars.push(process.env.CLEARDB_DATABASE);
+    connectVars.push(3306);
+}
 
 // Create connection
 const connection = mysql.createConnection({
     host: connectVars[0],
     user: connectVars[1],
     password: connectVars[2],
-    database: connectVars[3]
+    database: connectVars[3],
+    port: connectVars[4]
 });
 
 // Connect
