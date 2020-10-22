349ca1123f55521d913593eac17cd13742d5a89f
nbur4556 <nbur4556@gmail.com>
2020-10-22 18:10:59 -0500

    removed dev try catch for connection

--- config/connection.js
+++ config/connection.js
@@ -2,21 +2,21 @@ const mysql = require('mysql');
 
 const connectVars = new Array();
 
-try {
-    // If config file exists, push development credentials
-    const config = require('./config.json');
-    connectVars.push(config.host);
-    connectVars.push(config.user);
-    connectVars.push(config.password);
-    connectVars.push(config.database);
-}
-catch {
-    // Push deployment credentials
-    connectVars.push(process.env.CLEARDB_HOST);
-    connectVars.push(process.env.CLEARDB_USER);
-    connectVars.push(process.env.CLEARDB_PASSWORD);
-    connectVars.push(process.env.CLEARDB_DATABASE);
-}
+// try {
+//     // If config file exists, push development credentials
+//     const config = require('./config.json');
+//     connectVars.push(config.host);
+//     connectVars.push(config.user);
+//     connectVars.push(config.password);
+//     connectVars.push(config.database);
+// }
+// catch {
+// Push deployment credentials
+connectVars.push(process.env.CLEARDB_HOST);
+connectVars.push(process.env.CLEARDB_USER);
+connectVars.push(process.env.CLEARDB_PASSWORD);
+connectVars.push(process.env.CLEARDB_DATABASE);
+// }
 
 // Create connection
 const connection = mysql.createConnection({
